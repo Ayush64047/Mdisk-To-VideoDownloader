bbd9f11f2552188ef8d47e912a7f2cbf8a1bfbba
Aayush <bhaskarsharma64047@gmail.com>
2023-03-21 22:27:53 +0530

    Update main.py
    Signed-off-by: Aayush <bhaskarsharma64047@gmail.com>

--- main.py
+++ main.py
@@ -35,11 +35,11 @@ from mdisk import iswin
 def echo(client: pyrogram.client.Client, message: pyrogram.types.messages_and_media.message.Message):
 
     if not checkuser(message):
-        app.send_message(message.chat.id, '__You are either not **Authorized** or **Banned**__', reply_to_message_id=message.id,reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("📦 Channel ", url="https://t.me/WebXBots")]]))
+        app.send_message(message.chat.id, '__You are either not **Authorized** or **Banned**__', reply_to_message_id=message.id,reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("📦 Channel ", url="https://t.me/blackest_harbour")]]))
         return
 
-    app.send_message(message.chat.id, '**Hi, I am Mdisk Video Downloader, you can watch Videos without MX Player.\n__Send me a link to Start...__**',reply_to_message_id=message.id,
-    reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("📦 Channel ", url="https://t.me/WebXBots")]]))
+    app.send_message(message.chat.id, '**Hi, I am Mdisk Video Downloader, you can watch Videos without MX Player.\n__Send me a link from @MdiskLinkSearchBot to Start...__**',reply_to_message_id=message.id,
+    reply_markup=InlineKeyboardMarkup([[ InlineKeyboardButton("📦Bots Channel ", url="https://t.me/mdisk_bots")]]))
 
 # help command
 @app.on_message(filters.command(["help"]))
